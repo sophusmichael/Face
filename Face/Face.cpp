d99d35aa360b75cb0fdd3e6dcc8fbc68371460c6
sophusmichael <sophusmichael@gmail.com>
2015-10-18 22:01:36 -0700

    updated code to train and draw green boxes!
    updated code to train and draw green boxes!

--- Face/Face.cpp
+++ Face/Face.cpp
@@ -1,5 +1,4 @@
 
-
 #include "stdafx.h"
 #include <stdio.h>
 #include <vector>
@@ -42,9 +41,8 @@ int main(int argc, const char *argv[]) {
 
 
 	VideoCapture cap0(0);
-	string fn_haar = string(argv[1]);
-	string fn_csv = string(argv[2]);
-	int deviceId = atoi(argv[3]);
+	string fn_haar = "C:\\opencv\\sources\\data\\haarcascades\\haarcascade_frontalface_default.xml";
+	string fn_csv = "C:\\Users\\michael\\Documents\\att\\csvfile.csv";
 
 
 	vector<Mat> images;
@@ -54,129 +52,60 @@ int main(int argc, const char *argv[]) {
 	}
 	catch (cv::Exception& e) {
 		cerr << "Error opening file \"" << fn_csv << "\". Reason: " << e.msg << endl;
-		// nothing more we can do
 		exit(1);
 	}
-	// Get the height from the first image. We'll need this
-	// later in code to reshape the images to their original
-	// size AND we need to reshape incoming faces to this size:
+
 	int im_width = images[0].cols;
 	int im_height = images[0].rows;
 	// Create a FaceRecognizer and train it on the given images:
 	Ptr<FaceRecognizer> model = createFisherFaceRecognizer();
 	model->train(images, labels);
-	// later: 
-	while (cap0.isOpened()) {
-		Mat frame0;
-		Mat img;
-		Mat gray;
-		cap0.read(frame0);
-		if (cv::waitKey(30) >= 0) break;
-		// stereo processing here
 
-		if (count % 30 == 0)
-		{
-			cap0 >> img;
-
-			CascadeClassifier faceDetector;
-			try {
-				faceDetector.load("C:\\opencv\\sources\\data\\lbpcascades\\lbpcascade_frontalface.xml");
-
-			}
-			catch (cv::Exception e){}
-			if (faceDetector.empty()) {
-				cerr << "Error: Couldn't load Face Detector (";
-				cerr << "lbpcascade_frontalface.xml" << ")!" << endl;
-				exit(1);
-			}
+	CascadeClassifier haar_cascade;
+	haar_cascade.load(fn_haar);
+	// Get a handle to the Video device:
 
 
-			if (img.channels() == 3) {
-				cvtColor(img, gray, CV_BGR2GRAY);
-			}
-			else if (img.channels() == 4) {
-				cvtColor(img, gray, CV_BGRA2GRAY);
-			}
-			else {
-				gray = img;
-			}
-
-			const int DETECTION_WIDTH = 320;
-			Mat smallImg;
-			float scale = img.cols / (float)DETECTION_WIDTH;
-			if (img.cols > DETECTION_WIDTH) {
-				int scaledHeight = cvRound(img.rows / scale);
-				resize(gray, smallImg, Size(DETECTION_WIDTH, scaledHeight));
-			}
-			else {
-				smallImg = gray;
-			}
-
-			Mat equalizedImg;
-			equalizedImg;
-			equalizeHist(smallImg, equalizedImg);
-			img = equalizedImg;
-
-			int flags = CASCADE_SCALE_IMAGE;
-			Size minFeatureSize(20, 20);
-			float searchScaleFactor = 1.1f;
-			int minNeighbors = 4;
-
-			std::vector<Rect>faces;
-
-			faceDetector.detectMultiScale(equalizedImg, faces, searchScaleFactor, minNeighbors, flags, minFeatureSize);
-
-			if (img.cols > DETECTION_WIDTH) {
-				for (int i = 0; i < (int)faces.size(); i++) {
-					faces[i].x = cvRound(faces[i].x * scale);
-					faces[i].y = cvRound(faces[i].y * scale);
-					faces[i].width = cvRound(faces[i].width * scale);
-					faces[i].height = cvRound(faces[i].height * scale);
+			Mat frame;
+			for (;;) {
+				cap0 >> frame;
+				// Clone the current frame:
+				Mat original = frame.clone();
+				// Convert the current frame to grayscale:
+				Mat gray;
+				cvtColor(original, gray, CV_BGR2GRAY);
+				// Find the faces in the frame:
+				vector< Rect_<int> > faces;
+				haar_cascade.detectMultiScale(gray, faces);
+				for (int i = 0; i < faces.size(); i++) {
+					// Process face by face:
+					Rect face_i = faces[i];
+					// Crop the face from the image. So simple with OpenCV C++:
+					Mat face = gray(face_i);
+					Mat face_resized;
+					cv::resize(face, face_resized, Size(im_width, im_height), 1.0, 1.0, INTER_CUBIC);
+					// Now perform the prediction, see how easy that is:
+					int prediction = model->predict(face_resized);
+					// And finally write all we've found out to the original image!
+					// First of all draw a green rectangle around the detected face:
+					rectangle(original, face_i, CV_RGB(0, 255, 0), 1);
+					// Create the text we will annotate the box with:
+					string box_text = format("Prediction = %d", prediction);
+					// Calculate the position for annotated text (make sure we don't
+					// put illegal values in there):
+					int pos_x = std::max(face_i.tl().x - 10, 0);
+					int pos_y = std::max(face_i.tl().y - 10, 0);
+					// And now put it into the image:
+					putText(original, box_text, Point(pos_x, pos_y), FONT_HERSHEY_PLAIN, 1.0, CV_RGB(0, 255, 0), 2.0);
 				}
+				// Show the result:
+				imshow("face_recognizer", original);
+				// And display it:
+				char key = (char)waitKey(20);
+				// Exit this loop on escape:
+				if (key == 27)
+					break;
 			}
 
-
-			for (int i = 0; i < (int)faces.size(); i++) {
-				if (faces[i].x < 0)
-					faces[i].x = 0;
-				if (faces[i].y < 0)
-					faces[i].y = 0;
-				if (faces[i].x + faces[i].width > img.cols)
-					faces[i].x = img.cols - faces[i].width;
-				if (faces[i].y + faces[i].height > img.rows)
-					faces[i].y = img.rows - faces[i].height;
-			}
-
-			for (size_t i = 0; i < faces.size(); i++)
-			{
-				Point center(faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5);
-				ellipse(img, center, Size(faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar(255, 0, 255), 4, 8, 0);
-
-				Rect face_i = faces[i];
-				// Crop the face from the image. So simple with OpenCV C++:
-				Mat face = gray(face_i);
-				Mat face_resized;
-				cv::resize(face, face_resized, Size(im_width, im_height), 1.0, 1.0, INTER_CUBIC);
-				// Now perform the prediction, see how easy that is:
-				int prediction = model->predict(face_resized);
-			}
-
-			imshow("gray", img);
-
-		}
-		count++;
-	}
-
-
-
-
-
-
-
-
-
-	waitKey(0);
-
-
-
+	return 0;
 }
