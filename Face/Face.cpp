cb409f8725a0b4c270fdae1b838f92c3b779cbfb
sophusmichael <sophusmichael@gmail.com>
2015-10-26 00:32:49 -0700

    fixed some bugs
    fixed some bugs

--- Face/Face.cpp
+++ Face/Face.cpp
@@ -29,20 +29,26 @@ std::wstring s2ws(const std::string& s)
 	delete[] buf;
 	return r;
 }
-static void read_csv(const string& filename, vector<Mat>& images, vector<int>& labels, char separator = ';') {
+static void read_csv(const string& filename, vector<Mat>& images, vector<int>& labels, vector<string>& names, char separator = ';') {
 	std::ifstream file(filename.c_str(), ifstream::in);
+
+	int temp_int = -1;
 	if (!file) {
 		string error_message = "No valid input file was given, please check the given filename.";
 		CV_Error(CV_StsBadArg, error_message);
 	}
-	string line, path, classlabel;
+	string line, path, classlabel, namelabel;
 	while (getline(file, line)) {
 		stringstream liness(line);
 		getline(liness, path, separator);
-		getline(liness, classlabel);
+
+		getline(liness, classlabel, separator);
+		getline(liness, namelabel, separator);
 		if (!path.empty() && !classlabel.empty()) {
 			images.push_back(imread(path, 0));
 			labels.push_back(atoi(classlabel.c_str()));
+			names.push_back(namelabel);
+			cout << namelabel;
 		}
 	}
 }
@@ -52,7 +58,6 @@ static void read_csv(const string& filename, vector<Mat>& images, vector<int>& l
 int main(int argc, const char *argv[]) {
 	int count = 0; //variable for timer
 
-
 	VideoCapture cap0(0);
 	string fn_haar = "C:\\opencv\\sources\\data\\haarcascades\\haarcascade_frontalface_default.xml";
 	string fn_csv = "C:\\Users\\michael\\Documents\\att\\csvfile.csv";
@@ -60,20 +65,30 @@ int main(int argc, const char *argv[]) {
 
 	vector<Mat> images;
 	vector<int> labels;
+	vector<string> names;
+	std::map<int, std::string> integer_to_name;
 	try {
-		read_csv(fn_csv, images, labels);
+		read_csv(fn_csv, images, labels, names);
 	}
 	catch (cv::Exception& e) {
 		cerr << "Error opening file \"" << fn_csv << "\". Reason: " << e.msg << endl;
 		exit(1);
 	}
 
+	//create a map to map integer labels to string names
+	for (int i = 0; i < names.size(); i++){
+		int current_label = labels[i];
+		integer_to_name[current_label] = names[i];
+	}
+
+
 	int im_width = images[0].cols;
 	int im_height = images[0].rows;
 	// Create a FaceRecognizer and train it on the given images:
 	Ptr<FaceRecognizer> model = createFisherFaceRecognizer(0,1000);
 	model->train(images, labels);
-
+	//index for next face
+	int new_index = labels.back() + 1;
 	//model->load("C:\\Users\\michael\\Documents\\att\\fisherfaces_at.yml");
 
 	CascadeClassifier haar_cascade;
@@ -111,8 +126,8 @@ int main(int argc, const char *argv[]) {
 							stringstream ss;
 							ss << j+1;
 							string index = ss.str();
-							String subject_name = "joey\\";
-							string image = "image";
+							String subject_name = "michael";
+							string image = "\\";
 							string jpg = ".jpg";
 							string directory = "C:\\Users\\michael\\Documents\\att\\";
 							string newdir = directory + subject_name;
@@ -127,16 +142,19 @@ int main(int argc, const char *argv[]) {
 							j++;
 
 							images.push_back(face_resized);
-							labels.push_back(40);
+							labels.push_back(new_index);
+							names.push_back(subject_name);
 
 							if (j == 19){
 								cout << "retraining";
 								model->train(images, labels);
+								//we must update the map as well
+								for (int i = 0; i < labels.size(); i++){
+									int current_label = labels[i];
+									integer_to_name[current_label] = subject_name;
+								}
 							}
-					//		vector<Mat> newImages;
-						//	vector<int> newLabels;
 
-							//model->update(newImages, newLabels);
 					}
 
 
@@ -147,7 +165,15 @@ int main(int argc, const char *argv[]) {
 					// First of all draw a green rectangle around the detected face:
 					rectangle(original, face_i, CV_RGB(0, 255, 0), 1);
 					// Create the text we will annotate the box with:
-					string box_text = format("Prediction = %d", prediction);
+
+					string it; 
+					it = integer_to_name[prediction];
+					if (prediction == -1){
+						it = "unknown";
+					}
+					cout << it;
+					string box_text = "Prediction = " + it;
+					//string box_text = format("Prediction = ", boxname);
 					// Calculate the position for annotated text (make sure we don't
 					// put illegal values in there):
 					int pos_x = std::max(face_i.tl().x - 10, 0);
